e20220018c3959eccd5db88a49a8b3e3b976e619
UtopiaLSY <lisyu618@163.com>
2019-07-31 14:13:16 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/* main.cpp */
+#include <QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	MainWindow main;
+	main.show();
+	return app.exec();
+}
---
+++ screen.h
@@ -0,0 +1,46 @@
+/* screen.h */  
+#ifndef SCREEN_H 
+#define SCREEN_H 
+#include <QWidget> 
+#include "snake.h" 
+class Snake; 
+class QTimer; 
+class QLabel; 
+class QPushButton; 
+
+class Screen : public QWidget 
+{ 
+	Q_OBJECT; 
+	public: 
+		Screen(QWidget *parent = 0); 
+		~Screen(void); 
+	private slots: 
+		void my_timeout(void); 
+		void startGame(void); 
+		void pauseGame(void); 
+		void continueGame(void); 
+		void speedUp(void); 
+		void speedDown(void); 
+		void upClicked(void);        /* 处理按钮 Up 点击的槽 */ 
+		void downClicked(void);       /* 处理按钮 Down 点击的槽 */ 
+		void rightClicked(void);       /* 处理按钮 Right 点击的槽 */ 
+		void leftClicked(void);        /* 处理按钮 Left 点击的槽 */ 
+	private: 
+		void paintEvent(QPaintEvent *event);     /* 更新游戏界面 */ 
+		void init_var(void);        /* 初始化变量 */ 
+		void keyPressEvent(QKeyEvent *event);    /* 处理键盘事件的函数*/ 
+		QLabel *scoreLabel;        /* 显示得分的标签 */ 
+		QPushButton *upButton;       /* 向上按钮 */ 
+		QPushButton *downButton;       /* 向下按钮 */ 
+		QPushButton *leftButton;       /* 向左按钮 */ 
+		QPushButton *rightButton;       /* 向右按钮 */ 
+		Snake snake; 
+		bool IsDie; 
+		bool IsWin; 
+		bool IsRun; 
+		bool IsPause; 
+		QTimer *timer;         /* 定时器，用于设置更新时间 */ 
+		int times; 
+		int score; 
+}; 
+#endif  
